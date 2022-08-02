f1055ddc54fba3bc319aa24c411cd786d6204998
BARAHAVE <107971011+BARAHAVE@users.noreply.github.com>
2022-08-02 16:55:23 +0700

    Update ModalPractice.js

--- components/ModalPractice.js
+++ components/ModalPractice.js
@@ -1,50 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, Pressable, View , SafeAreaView} from "react-native";
 
 const ModalPractice = () => {
-
-    const [showModal ,setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
-    <SafeAreaView style = {{flex:1}}>
-    <View style = {styles.centeredView}>
-        <Modal 
-            animationType={'slide'}
-            transparent={false}
-            visible={showModal}
-            onRequestClose={()=>alert('Modal has been closed.')}
-        > 
-            <View style = {styles.modalView}>
-                <Text style={styles.modalText}>Modal is Open!</Text>
-                <Button 
-                    title="Click to Close Modal"
-                    onPress={()=>setShowModal(!showModal)}
-                />
-
-            </View>
-
-        </Modal>
-
-        <Button
-        title = 'Click to Open Modal'
-        onPress={()=>setShowModal(!showModal)}
-        />
-
-
-    </View>
+    <SafeAreaView style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setShowModal(!showModal);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>คุณลืมปิดไฟในห้องT_T</Text>
+            <Pressable
+              style={[styles.button, styles.buttonOpen]}
+              onPress={() => setShowModal(!showModal)}>
+              <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อปิดไฟ</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      
+      <Text style={styles.modalText}>ไฟในห้องปิดอยู่</Text>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setShowModal(true)}
+      >
+        <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อเปิดไฟอีกครั้ง</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
-
-export default ModalPractice;
-
-
 
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22,
+    marginTop: 50
   },
   modalView: {
     margin: 20,
@@ -53,14 +51,203 @@ const styles = StyleSheet.create({
     padding: 35,
     alignItems: "center",
     shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5,
+    elevation: 5
   },
-  button: { borderRadius: 20, padding: 10, elevation: 2 },
-  buttonOpen: { backgroundColor: "#F194FF" },
-  buttonClose: { backgroundColor: "#2196F3" },
-  textStyle: { color: "white", fontWeight: "bold", textAlign: "center" },
-  modalText: { marginBottom: 15, textAlign: "center" },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 1,
+    textAlign: "center",
+    color: "black"
+  }
 });
+
+export default ModalPractice;import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, Pressable, View , SafeAreaView} from "react-native";
+
+const ModalPractice = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <SafeAreaView style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setShowModal(!showModal);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>คุณลืมปิดไฟในห้องT_T</Text>
+            <Pressable
+              style={[styles.button, styles.buttonOpen]}
+              onPress={() => setShowModal(!showModal)}>
+              <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อปิดไฟ</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      
+      <Text style={styles.modalText}>ไฟในห้องปิดอยู่</Text>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setShowModal(true)}
+      >
+        <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อเปิดไฟอีกครั้ง</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 1,
+    textAlign: "center",
+    color: "black"
+  }
+});
+
+export default ModalPractice;import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, Pressable, View , SafeAreaView} from "react-native";
+
+const ModalPractice = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <SafeAreaView style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showModal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setShowModal(!showModal);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>คุณลืมปิดไฟในห้องT_T</Text>
+            <Pressable
+              style={[styles.button, styles.buttonOpen]}
+              onPress={() => setShowModal(!showModal)}>
+              <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อปิดไฟ</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      
+      <Text style={styles.modalText}>ไฟในห้องปิดอยู่</Text>
+      <Pressable
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => setShowModal(true)}
+      >
+        <Text style={styles.textStyle}>กรุณากดปุ่มเพื่อเปิดไฟอีกครั้ง</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 1,
+    textAlign: "center",
+    color: "black"
+  }
+});
+
+export default ModalPractice;
